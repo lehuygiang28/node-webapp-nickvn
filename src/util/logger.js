523a37b55b731538d184878d8e7e7302081b5c9a
lehuygiang28 <lehuygiang28@gmail.com>
2022-12-27 19:22:53 +0700

    Add comment

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,5 +1,5 @@
 const logger = require('node-color-log');
-// Set date format to 
+// Set date format to mm/dd/yyyy h/mm/ss
 logger.setDate(() => (new Date()).toLocaleString().slice(0, 19).replace(/-/g, "/").replace("T", " "));
 
 module.exports = {logger}
