836bc3f533f23da93704585cb76048728ad03ecc
lehuygiang28 <lehuygiang28@gmail.com>
2022-12-28 17:40:41 +0700

    Set autoincrement for product_id with mongoose-sequence lib

--- src/app/models/LienMinh.js
+++ src/app/models/LienMinh.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
 
 const LienMinh = new Schema({
-    product_id: {type: Number, minLength: 1},
-    userName: {type: String, minLength: 1},
-    password: {type: String, minLength: 1},
-    price: {type: Number, min: 0},
-    champ: {type: Number, min: 0},
-    skin: {type: Number, min: 0},
-    rank: {type: String, minLength: 1},
-    status_account: {type: String, minLength: 1},
-    note: {type: String, minLength: 0},
-    status_id: {type: Number, minLength: 1},
-    status_name: {type: String, minLength: 1},
-    img: {type: Array, minLength: 1}
+    product_id: { type: Number, require: true },
+    userName: { type: String, minLength: 1 },
+    password: { type: String, minLength: 1 },
+    price: { type: Number, min: 0 },
+    champ: { type: Number, min: 0 },
+    skin: { type: Number, min: 0 },
+    rank: { type: String, minLength: 1 },
+    status_account: { type: String, minLength: 1 },
+    note: { type: String, minLength: 0 },
+    status_id: { type: Number, minLength: 1 },
+    status_name: { type: String, minLength: 1 },
+    img: { type: Array, minLength: 1 }
+}, {
+    product_id: false
 });
 
-
+LienMinh.plugin(AutoIncrement, { inc_field: 'product_id' });
 module.exports = mongoose.model('LienMinh', LienMinh);
--- src/util/project_extensions.js
+++ src/util/project_extensions.js
@@ -12,7 +12,6 @@ function generateLienMinh(counters, next) {
         "/img/default-thumb6.webp",
         "/img/0qBPw7AiOQ_1632531413.jpg"
     ];
-
     const afterImg = [
         "/img/CM9q56zAnM_1632531413.jpg",
         "/img/pTWDgoJQuz_1632531413.jpg",
@@ -20,9 +19,7 @@ function generateLienMinh(counters, next) {
         "/img/HLpEr7ojZm_1632531414.jpg",
         "/img/xxuC88f0h9_1632531414.jpg"
     ];
-
     const rankRd = ["Chưa Rank", "Sắt", "Đồng", "Bạc", "Vàng", "Bạch Kim", "Kim Cương", "Cao Thủ", "Đại Cao Thủ", "Thách Đấu"];
-
     const NOT_SOLD = 1005;
 
     function randomString(length) {
@@ -38,39 +35,37 @@ function generateLienMinh(counters, next) {
     function randomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    // Get last product_id from database
-    LienMinh.findOne({ status_id: 1005 })
-        .sort('-product_id')
-        .then(async lienMinh => {
-            let lastImg = lienMinh.product_id;
-            console.log(`Max product_id ${lastImg}`);
-            for (let i = 0; i < counters; i++) {
-                lastImg++;
-                let product = new LienMinh({
-                    product_id: lastImg,
-                    userName: 'shop' + randomString(6),
-                    password: 'passw' + randomString(8),
-                    price: randomNumber(10000, 20000000),
-                    champ: randomNumber(20, 199),
-                    skin: randomNumber(50, 600),
-                    rank: rankRd[randomNumber(0, rankRd.length - 1)],
-                    status_account: 'Trắng thông tin',
-                    note: 'note',
-                    status_id: NOT_SOLD,
-                    status_name: 'Chưa bán',
-                    img: [
-                        firstImg[randomNumber(0, firstImg.length - 1)]
-                    ]
-                });
-                afterImg.forEach(element => {
-                    product.img.push(element);
-                });
-                // console.log(product);
+
+    for (let i = 0; i < counters; i++) {
+        let product = new LienMinh({
+            userName: 'shop' + randomString(6),
+            password: 'passw' + randomString(8),
+            price: randomNumber(10000, 20000000),
+            champ: randomNumber(20, 199),
+            skin: randomNumber(50, 600),
+            rank: rankRd[randomNumber(0, rankRd.length - 1)],
+            status_account: 'Trắng thông tin',
+            note: 'note',
+            status_id: NOT_SOLD,
+            status_name: 'Chưa bán',
+            img: [
+                firstImg[randomNumber(0, firstImg.length - 1)]
+            ]
+        });
+
+        // Concat() is better than '...' (spread syntax ES6)
+        // product.img.push(...afterImg);
+        product.img = product.img.concat(afterImg);
+
+        product.save()
+            .then(() => {
+                logger.info(`Add product successfully!`);
                 logger.info(product);
-                await product.save();
-            }
-        })
-        .catch(next);
+            })
+            .catch(next);
+    }
+    // })
+    // .catch(next);
 }
 
 function resetProductAndUserPuchased(next) {
