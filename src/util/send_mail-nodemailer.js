82cf653b61025f031bda54e0f11dbb2923efab6c
lehuygiang28 <lehuygiang28@gmail.com>
2023-01-01 23:00:59 +0700

    Change email template

--- src/util/send_mail-nodemailer.js
+++ src/util/send_mail-nodemailer.js
@@ -1,5 +1,7 @@
+const console = require('console');
 const nodemailer = require('nodemailer');
 const util = require('util');
+const { emailTemplate } = require('./email-template');
 
 /***
  * Send an email to the user with the callback
@@ -22,24 +24,44 @@ async function sendMail(to, inputContent) {
             pass: process.env.EMAIL_APP_PASSWORD
         }
     });
-    var content = '';
-    content += `
-        <div style="padding: 10px; background-color: #003375">
-            <div style="padding: 10px; background-color: white;">
-                <h4 style="color: #0085ff">${inputContent.title}</h4>
-                <span style="color: black">
-                    ${inputContent.context}
-                </span>
-            </div>
-        </div>
-    `;
+    // var content = '';
+    // content += `
+    //     <div style="padding: 10px; background-color: #003375">
+    //         <div style="padding: 10px; background-color: white;">
+    //             <h4 style="color: #0085ff">${inputContent.title}</h4>
+    //             <span style="color: black">
+    //                 ${inputContent.context}
+    //             </span>
+    //         </div>
+    //     </div>
+    // `;
+
+    let mailTemplateHtml = emailTemplate(inputContent);
+    console.log(mailTemplateHtml);
 
     client.sendMail({
         from: "Sender",
         to: to,
         subject: inputContent.subject,
         // text: "",
-        html: content
+        html: mailTemplateHtml,
+        attachments: [{
+            filename: 'image-1.png',
+            path: 'src/private/email-img/image-1.png',
+            cid: 'image-1'
+        }, {
+            filename: 'image-2.png',
+            path: 'src/private/email-img/image-2.png',
+            cid: 'image-2'
+        }, {
+            filename: 'image-3.png',
+            path: 'src/private/email-img/image-3.png',
+            cid: 'image-3'
+        }, {
+            filename: 'image-4.png',
+            path: 'src/private/email-img/image-4.png',
+            cid: 'image-4'
+        }]
     });
     console.log(`Mail sent successful to: ${to}`);
     console.log(inputContent);
