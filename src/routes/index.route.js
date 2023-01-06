ab377b9f346e4fb210971766c0e5799b2f2feada
lehuygiang28 <lehuygiang28@gmail.com>
2023-01-06 23:28:27 +0700

    Add a error page

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -9,7 +9,16 @@ function route(app) {
     app.use('/user', userRouter);
     app.use('/lien-minh', lienMinhsRouter);
     app.use('/', siteRouter);
-
+    app.use('*', function(req, res, next) {
+        res.status(404).render('error/error', {
+            layout: false,
+            error: {
+                code: 404,
+                title: 'Page Not Found',
+                message: 'We’re sorry, the page you have looked for does not exist in our website! Maybe go to our home page or try to use a search ?'
+            }
+        });
+    });
 }
 
 module.exports = route;
