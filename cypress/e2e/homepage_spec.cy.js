34736778dbb57b713e6df38918e023e6bfc3abd0
lehuygiang28 <lehuygiang28@gmail.com>
2023-01-31 14:38:14 +0700

    Refactor fix action link and selector

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -68,75 +68,82 @@
  * Load from home page to buy product with login
  */
 describe('Home page load to buy with login', function () {
+
     before(() => {
-        cy.fixture('user')
-            .then((user) => {
-                this.user = user;
-            })
-            .then((user) => {
-                let userMoney = {};
-                let userNoMoney = {};
+        cy.fixture('user').then((user) => {
+            this.user = user;
 
-                Object.assign(userMoney, user.user_with_money);
-                userMoney.password_type = undefined;
+            let userMoney = {};
+            let userNoMoney = {};
 
-                Object.assign(userNoMoney, user.user_without_money);
-                userNoMoney.password_type = undefined;
+            Object.assign(userMoney, user.user_with_money);
+            userMoney.password_type = undefined;
 
-                const userInserts = [userMoney, userNoMoney];
-                cy.dropCollection('userpuchaseds').then((res) => {
-                    cy.log(res);
-                });
-                cy.dropCollection('users').then((res) => {
-                    cy.log(res);
-                });
-                cy.dropCollection('lienminhs').then((res) => {
-                    cy.log(res);
-                });
+            Object.assign(userNoMoney, user.user_without_money);
+            userNoMoney.password_type = undefined;
+
+            const userInserts = [userMoney, userNoMoney];
+
+            cy.dropCollection('users').then((res) => {
+                cy.log(res);
                 cy.createCollection('users');
                 cy.insertMany(userInserts, { collection: 'users' }).then((res) => {
                     console.log(res);
                 });
             });
+        });
 
         cy.fixture('lienminhs').then(async (product) => {
-            await cy.dropCollection('lienminhs', { failSilently: true }).then((res) => {
-                cy.log(res);
+            await Promise.all([
+                cy.dropCollection('lienminhs', { failSilently: true }),
+                cy.dropCollection('counters', { failSilently: true }),
+            ]).then(async ([res1, res2]) => {
+                //Destructuring the array of responses from the promises into two variables res1 and res2  respectively
+                cy.log(res1); //Logging the response of the first promise i-e dropCollection('lienminhs')
+                cy.log(res2); //Logging the response of the second promise i-e dropCollection('counters')
+
+                await Promise.all([
+                    cy.createCollection('counters'),
+                    cy.createCollection('lienminhs'),
+                ]); //Creating both collections in parallel using Promise all
+
+                let counter = {
+                    //Defining counter object to be inserted into counters collection
+                    id: 'product_id', //Setting id field to product_id
+                    reference_value: null, //Setting reference value to null
+                    seq: 1, //Setting seq field to 1
+                };
+
+                await Promise.all([
+                    cy.insertOne(counter, { collection: 'counters' }),
+                    cy.insertOne(product, { collection: 'lienminhs' }),
+                ]); //Inserting both counter and product documents in parallel using Promise all
             });
-            await cy.dropCollection('counters', { failSilently: true }).then((res) => {
-                cy.log(res);
-            });
-            await cy.createCollection('counters');
-            await cy.createCollection('lienminhs');
-            let counter = {
-                id: 'product_id',
-                reference_value: null,
-                seq: 1,
-            };
-            await cy.insertOne(counter, { collection: 'counters' });
-            await cy.insertOne(product, { collection: 'lienminhs' });
         });
     });
 
     it('passes home page', () => {
         cy.visit('/');
+        cy.contains('Trang chủ');
     });
 
     it('pass login page', () => {
         cy.visit('/');
 
-        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/login"]', 'Đăng nhập').click();
+        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/dang-nhap"]', 'Đăng nhập').click();
         cy.url().should('include', 'dang-nhap');
     });
 
     it('pass login form', () => {
         cy.visit('/');
 
-        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/login"]', 'Đăng nhập').click();
+        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/dang-nhap"]', 'Đăng nhập').click();
 
-        cy.get('.login-box-body.box-custom>form>div>input[name="username"]').type('tester');
-        cy.get('.login-box-body.box-custom>form>div>input[name="password"]').type('1');
-        cy.get('.login-box-body.box-custom>form>div>div>button[type="submit"]').click();
+        cy.get('form[action="/dang-nhap"]').within(() => {
+            cy.get('input[name="username"]').type('tester');
+            cy.get('input[name="password"]').type('1');
+            cy.get('button[type="submit"]').click();
+        });
 
         cy.url().should('contain', '/');
         cy.contains('tester');
@@ -146,15 +153,14 @@ describe('Home page load to buy with login', function () {
     it('pass to product details page and click buy without money', () => {
         cy.visit('/');
 
-        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/login"]', 'Đăng nhập').click();
+        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/dang-nhap"]', 'Đăng nhập').click();
 
-        cy.get('.login-box-body.box-custom>form>div>input[name="username"]').type(
-            this.user.user_without_money.userName,
-        );
-        cy.get('.login-box-body.box-custom>form>div>input[name="password"]').type(
-            this.user.user_without_money.password_type,
-        );
-        cy.get('.login-box-body.box-custom>form>div>div>button[type="submit"]').click();
+        cy.get('form[action="/dang-nhap"]').within(() => {
+            cy.get('input[name="username"]').type(this.user.user_without_money.userName);
+            cy.get('input[name="password"]').type(this.user.user_without_money.password_type);
+            cy.get('button[type="submit"]').click();
+        });
+
         cy.contains('.classWithPad>div>h2>a', 'Liên Minh Huyền Thoại').click();
 
         cy.contains('.classWithPad > .news_title > a', 'Liên Minh').click();
@@ -186,22 +192,23 @@ describe('Home page load to buy with login', function () {
     it('pass to product details page and click buy with money', () => {
         cy.visit('/');
 
-        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/login"]', 'Đăng nhập').click();
+        cy.contains('.nav.navbar-nav.c-theme-nav>li>a[href="/dang-nhap"]', 'Đăng nhập').click();
 
-        cy.get('.login-box-body.box-custom>form>div>input[name="username"]').type(
-            this.user.user_with_money.userName,
-        );
-        cy.get('.login-box-body.box-custom>form>div>input[name="password"]').type(
-            this.user.user_with_money.password_type,
-        );
-        cy.get('.login-box-body.box-custom>form>div>div>button[type="submit"]').click();
-        cy.contains('.classWithPad>div>h2>a', 'Liên Minh Huyền Thoại').click({force: true});
+        cy.get('form[action="/dang-nhap"]').within(() => {
+            cy.get('input[name="username"]').type(this.user.user_with_money.userName);
+            cy.get('input[name="password"]').type(this.user.user_with_money.password_type);
+            cy.get('button[type="submit"]').click();
+        });
 
-        cy.contains('div.classWithPad > .news_title > a', 'Liên Minh').click({force: true});
+        cy.contains('.classWithPad>div>h2>a', 'Liên Minh Huyền Thoại').click({ force: true });
+
+        cy.contains('div.classWithPad > .news_title > a', 'Liên Minh').click({ force: true });
 
         cy.contains('div.classWithPad > .image > a', 'MS: 1').click();
 
-        cy.contains('.c-product-header>.c-content-title-1>button','Mua ngay').click({force: true});
+        cy.contains('.c-product-header>.c-content-title-1>button', 'Mua ngay').click({
+            force: true,
+        });
         cy.contains('Xác nhận mua ngay');
         cy.contains('Chi tiết tài khoản #1');
         cy.contains('.nav.nav-justified > li > a', 'Thanh toán').click();
@@ -214,7 +221,6 @@ describe('Home page load to buy with login', function () {
         cy.contains('Xác nhận mua ngay');
         cy.contains('.nav.nav-justified > li > a', 'Tài khoản').click();
         cy.contains('Chi tiết tài khoản #1');
-
 
         cy.contains('.modal-content>form>.modal-footer>a', 'Xác nhận mua ngay').click();
         cy.contains('Mua tài khoản thành công, thông tin tài khoản đã được gửi về email của bạn!');
