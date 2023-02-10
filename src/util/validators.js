3789cc9dffe5fabf5f1432f2bd38fb706da214b2
lehuygiang28 <lehuygiang28@gmail.com>
2023-02-10 20:00:34 +0700

    Change var to let

--- src/util/validators.js
+++ src/util/validators.js
@@ -1,4 +1,4 @@
-var emailRegex =
+let emailRegex =
     /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
 /***
@@ -9,14 +9,14 @@ function isEmailValid(email) {
 
     if (email.length > 254) return false;
 
-    var valid = emailRegex.test(email);
+    let valid = emailRegex.test(email);
     if (!valid) return false;
 
     // Further checking of some things regex can't handle
-    var parts = email.split('@');
+    let parts = email.split('@');
     if (parts[0].length > 64) return false;
 
-    var domainParts = parts[1].split('.');
+    let domainParts = parts[1].split('.');
     if (
         domainParts.some(function (part) {
             return part.length > 63;
