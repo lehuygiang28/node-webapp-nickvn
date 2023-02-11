177829f8e88eb8cb451b74899b057301a57af9fe
lehuygiang28 <lehuygiang28@gmail.com>
2023-02-11 15:24:57 +0700

    Remove unused

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -7,16 +7,6 @@ const path = require('path');
  * @module connect
  */
 async function connect() {
-    // mongodb://localhost:27017/nodejs-nickvn
-    // try {
-    //   mongoose.set('strictQuery', false);
-    //   await mongoose.connect(process.env.MONGOURL,{useNewUrlParser:true, useUnifiedTopology: true });
-    //   console.log('Connect mongodb success');
-
-    // } catch (error) {
-    //   console.log('Connect mongodb Failed');
-    // }
-
     mongoose.set('strictQuery', false);
     await mongoose
         .connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
