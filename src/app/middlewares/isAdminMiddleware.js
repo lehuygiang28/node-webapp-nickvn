1aae411c701293bb208c444cad0b0fcdef45750a
lehuygiang28 <lehuygiang28@gmail.com>
2023-02-13 04:01:15 +0700

    Require authen admin

--- src/app/middlewares/isAdminMiddleware.js
+++ src/app/middlewares/isAdminMiddleware.js
@@ -7,14 +7,31 @@
  * @returns Redirect to login page if not already logged in, otherwise next();
  */
 function isAdmin(req, res, next) {
-    let nonCheck = ['/login', '/signout'];
+    let nonCheckSites = ['/login', '/signout'];
+    let nonCheckFeatures = ['/change-visible'];
 
-    if(req.path === nonCheck[0] && req.session.adminUser){
+    /**
+     * If the request is not authenticated, return error
+     */
+    if(req.originalUrl.includes(nonCheckFeatures) && !req.session.adminUser){
+        return res.json({error: 'You not allowed to access this page'});
+    }
+
+    /**
+     * If request is login site, return login page
+     */
+    if(req.path === nonCheckSites[0] && req.session.adminUser){
         return res.redirect('/admin');
     }
 
-    if(nonCheck.includes(req.path)) return next();
+    /**
+     * If request is include non check sites, next() will return
+     */
+    if(nonCheckSites.includes(req.path)) return next();
 
+    /**
+     * Require admin authen, if not return login page
+     */
     if (!req.session.adminUser) {
         return res.redirect('/admin/login');
     }
