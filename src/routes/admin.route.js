3b5ee7b637d566409c3bb7f8fe7c37a58858c85c
lehuygiang28 <lehuygiang28@gmail.com>
2023-02-23 06:19:15 +0700

    update product route

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -3,9 +3,16 @@ const router = express.Router();
 
 const AdminControllers = require('../app/controllers/AdminController');
 
+router.get('/users', AdminControllers.users);
+
+router.post('/products/:id/edit', AdminControllers.editProductSolvers)
+router.get('/products/:id/view', AdminControllers.detailProduct);
+router.post('/products/add', AdminControllers.addProductSolvers);
+router.get('/products/add', AdminControllers.addProduct);
+router.get('/products', AdminControllers.products);
+
 router.post('/categories/:id/change-visible', AdminControllers.changeCateVisible);
 router.post('/categories/:id/edit', AdminControllers.editCategorySolvers);
-// router.get('/categories/:id/edit', AdminControllers.editCategory);
 router.get('/categories/:id/view', AdminControllers.detailCategory);
 router.post('/categories/add', AdminControllers.addCategorySolvers);
 router.get('/categories/add', AdminControllers.addCategory);
