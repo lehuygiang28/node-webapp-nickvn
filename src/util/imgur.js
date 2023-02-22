c4b0eaa0d9b016a32149f420d2e4892d5af7a217
lehuygiang28 <lehuygiang28@gmail.com>
2023-02-22 16:15:25 +0700

    add function remove image

--- src/util/imgur.js
+++ src/util/imgur.js
@@ -1,4 +1,8 @@
 const { ImgurClient } = require('imgur');
+const client = new ImgurClient({
+    clientId: process.env.IMGUR_CLIENT_ID,
+    clientSecret: process.env.IMGUR_CLIENT_SECRET,
+});
 
 /***
  * An functionality to upload image to imgur
@@ -7,14 +11,26 @@ const { ImgurClient } = require('imgur');
  * @returns {string} The link of the image
  */
 async function UploadImage(img) {
-    const client = new ImgurClient({
-        clientId: process.env.IMGUR_CLIENT_ID,
-        clientSecret: process.env.IMGUR_CLIENT_SECRET,
-    });
     const response = await client.upload({
         image: img.data,
     });
     return response.data;
 }
 
-module.exports = { UploadImage };
+/***
+ * An functionality to remove image from imgur
+ *
+ * @param {string} deletehash The string of image hash to delete
+ * @returns {string} True if the image was successfully removed, otherwise false
+ */
+function DeleteImage(deletehash) {
+    try {
+        client.deleteImage(deletehash);
+        return true;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+}
+
+module.exports = { UploadImage, DeleteImage };
