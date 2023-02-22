fd9466533c25e91e6006d27ca1545b11e0b69bf2
lehuygiang28 <lehuygiang28@gmail.com>
2023-02-23 06:18:46 +0700

    update edit-able for products edit page

--- src/public/adminAssets/js/editable.js
+++ src/public/adminAssets/js/editable.js
@@ -1,169 +1,182 @@
+let arrAllowed = ['products', 'categories'];
+let hrefSplit = window.location.href.split('/');
+
 window.onload = function () {
-    if (window.location.href.includes('view'))
-        document.getElementById('content-editable').onclick = function (event) {
-            let dd_element, input, text;
+    if (
+        !hrefSplit || // Check validity of requested
+        hrefSplit.length <= 1 || // Check validity of requested
+        hrefSplit[3].toLowerCase() !== 'admin' || // Check controller is admin
+        hrefSplit[hrefSplit.length - 1].toLowerCase() !== 'view' || // Check last parameter is 'view
+        !arrAllowed.includes(hrefSplit[4]) // Check is allowed list to editable
+    ) {
+        return;
+    }
 
-            // Get the event (handle MS difference)
-            event = event || window.event;
+    document.getElementById('content-editable').onclick = function (event) {
+        let dd_element, input, text;
 
-            // Get the root element of the event (handle MS difference)
-            dd_element = event.target || event.srcElement;
+        // Get the event (handle MS difference)
+        event = event || window.event;
 
-            // If has a disable-editable, then return
-            if (dd_element.hasAttribute('disable-editable')) return;
+        // Get the root element of the event (handle MS difference)
+        dd_element = event.target || event.srcElement;
 
-            // If clicked on a dd element
-            if (dd_element && dd_element.tagName.toUpperCase() === 'DD') {
-                // Hide it
-                dd_element.style.display = 'none';
+        // If has a disable-editable, then return
+        if (dd_element.hasAttribute('disable-editable')) return;
 
-                // Get its text
-                text = dd_element.textContent;
+        // If clicked on a dd element
+        if (dd_element && dd_element.tagName.toUpperCase() === 'DD') {
+            // Hide it
+            dd_element.style.display = 'none';
 
-                // Create an input
-                input = document.createElement('input');
-                input.classList.add('form-control', 'w-50');
-                input.type = 'text';
-                input.value = text;
-                input.size = Math.max((text.length / 4) * 3, 4);
-                dd_element.parentNode.insertBefore(input, dd_element);
+            // Get its text
+            text = dd_element.textContent;
 
-                // Focus it, hook blur to undo
-                input.focus();
-                input.onblur = function () {
-                    // Remove the input
-                    dd_element.parentNode.removeChild(input);
+            // Create an input
+            input = document.createElement('input');
+            input.classList.add('form-control', 'w-50');
+            input.type = 'text';
+            input.value = text;
+            input.size = Math.max((text.length / 4) * 3, 4);
+            dd_element.parentNode.insertBefore(input, dd_element);
 
-                    // Update the span
-                    dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
-                    dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
+            // Focus it, hook blur to undo
+            input.focus();
+            input.onblur = function () {
+                // Remove the input
+                dd_element.parentNode.removeChild(input);
 
-                    // Show the span again
-                    dd_element.style.display = '';
-                };
-            }
+                // Update the span
+                dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
+                dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
 
-            // If clicked on a dd icon edit
-            if (dd_element && dd_element.parentNode.tagName.toUpperCase() === 'DD') {
-                // Change clicked on child to parent
-                dd_element = dd_element.parentNode;
+                // Show the span again
+                dd_element.style.display = '';
+            };
+        }
 
-                // Hide it
-                dd_element.style.display = 'none';
+        // If clicked on a dd icon edit
+        if (dd_element && dd_element.parentNode.tagName.toUpperCase() === 'DD') {
+            // Change clicked on child to parent
+            dd_element = dd_element.parentNode;
 
-                // Get its text
-                text = dd_element.textContent;
+            // Hide it
+            dd_element.style.display = 'none';
 
-                // Create an input
-                input = document.createElement('input');
-                input.classList.add('form-control', 'w-50');
-                input.type = 'text';
-                input.value = text;
-                input.size = Math.max((text.length / 4) * 3, 4);
-                dd_element.parentNode.insertBefore(input, dd_element);
+            // Get its text
+            text = dd_element.textContent;
 
-                // Focus it, hook blur to undo
-                input.focus();
-                input.onblur = function () {
-                    // Remove the input
-                    dd_element.parentNode.removeChild(input);
+            // Create an input
+            input = document.createElement('input');
+            input.classList.add('form-control', 'w-50');
+            input.type = 'text';
+            input.value = text;
+            input.size = Math.max((text.length / 4) * 3, 4);
+            dd_element.parentNode.insertBefore(input, dd_element);
 
-                    // Update the span
-                    dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
-                    dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
+            // Focus it, hook blur to undo
+            input.focus();
+            input.onblur = function () {
+                // Remove the input
+                dd_element.parentNode.removeChild(input);
 
-                    // Show the span again
-                    dd_element.style.display = '';
-                };
-            }
+                // Update the span
+                dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
+                dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
 
-            // If clicked on a select option ICON edit
-            if (dd_element && dd_element.tagName.toUpperCase() === 'DO') {
-                let visibleCase = ['show', 'hide'];
+                // Show the span again
+                dd_element.style.display = '';
+            };
+        }
 
-                // Hide it
-                dd_element.style.display = 'none';
+        // If clicked on a select option ICON edit
+        if (dd_element && dd_element.tagName.toUpperCase() === 'DO') {
+            let visibleCase = ['show', 'hide'];
 
-                // Get its text
-                text = dd_element.textContent;
+            // Hide it
+            dd_element.style.display = 'none';
 
-                // Create an select
-                input = document.createElement('select');
-                input.classList.add('form-select', 'text-capitalize', 'w-50');
-                input.setAttribute('aria-label', 'Select visibility status');
-                input.setAttribute('name', 'visible');
-                input.size = visibleCase.length;
+            // Get its text
+            text = dd_element.textContent;
 
-                for (const element of visibleCase) {
-                    let option = document.createElement('option');
-                    if (text === element) {
-                        option.setAttribute('selected', '');
-                    }
-                    option.value = element;
-                    option.textContent = element;
-                    input.appendChild(option);
+            // Create an select
+            input = document.createElement('select');
+            input.classList.add('form-select', 'text-capitalize', 'w-50');
+            input.setAttribute('aria-label', 'Select visibility status');
+            input.setAttribute('name', 'visible');
+            input.size = visibleCase.length;
+
+            for (const element of visibleCase) {
+                let option = document.createElement('option');
+                if (text === element) {
+                    option.setAttribute('selected', '');
                 }
-                dd_element.parentNode.insertBefore(input, dd_element);
-
-                input.focus();
-                input.onblur = function () {
-                    // Remove the input
-                    dd_element.parentNode.removeChild(input);
-
-                    // Update the span
-                    dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
-                    dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
-
-                    // Show the span again
-                    dd_element.style.display = '';
-                };
+                option.value = element;
+                option.textContent = element;
+                input.appendChild(option);
             }
+            dd_element.parentNode.insertBefore(input, dd_element);
 
-            // If clicked on a select option ICON edit
-            if (dd_element && dd_element.parentNode.tagName.toUpperCase() === 'DO') {
-                let visibleCase = ['show', 'hide'];
+            input.focus();
+            input.onblur = function () {
+                // Remove the input
+                dd_element.parentNode.removeChild(input);
 
-                // Change clicked on child to parent
-                dd_element = dd_element.parentNode;
+                // Update the span
+                dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
+                dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
 
-                // Hide it
-                dd_element.style.display = 'none';
+                // Show the span again
+                dd_element.style.display = '';
+            };
+        }
 
-                // Get its text
-                text = dd_element.textContent;
+        // If clicked on a select option ICON edit
+        if (dd_element && dd_element.parentNode.tagName.toUpperCase() === 'DO') {
+            let visibleCase = ['show', 'hide'];
 
-                // Create an select
-                input = document.createElement('select');
-                input.classList.add('form-select', 'text-capitalize', 'w-50');
-                input.setAttribute('aria-label', 'Select visibility status');
-                input.setAttribute('name', 'visible');
-                input.size = visibleCase.length;
+            // Change clicked on child to parent
+            dd_element = dd_element.parentNode;
 
-                for (const element of visibleCase) {
-                    let option = document.createElement('option');
-                    if (text === element) {
-                        option.setAttribute('selected', '');
-                    }
-                    option.value = element;
-                    option.textContent = element;
-                    input.appendChild(option);
+            // Hide it
+            dd_element.style.display = 'none';
+
+            // Get its text
+            text = dd_element.textContent;
+
+            // Create an select
+            input = document.createElement('select');
+            input.classList.add('form-select', 'text-capitalize', 'w-50');
+            input.setAttribute('aria-label', 'Select visibility status');
+            input.setAttribute('name', 'visible');
+            input.size = visibleCase.length;
+
+            for (const element of visibleCase) {
+                let option = document.createElement('option');
+                if (text === element) {
+                    option.setAttribute('selected', '');
                 }
-                dd_element.parentNode.insertBefore(input, dd_element);
-
-                input.focus();
-                input.onblur = function () {
-                    // Remove the input
-                    dd_element.parentNode.removeChild(input);
-
-                    // Update the span
-                    dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
-                    dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
-
-                    // Show the span again
-                    dd_element.style.display = '';
-                };
+                option.value = element;
+                option.textContent = element;
+                input.appendChild(option);
             }
-        };
+            dd_element.parentNode.insertBefore(input, dd_element);
+
+            input.focus();
+            input.onblur = function () {
+                // Remove the input
+                dd_element.parentNode.removeChild(input);
+
+                // Update the span
+                dd_element.innerHTML = input.value == '' ? '&nbsp;' : input.value;
+                dd_element.innerHTML += '<i class="fas fa-edit float-end"></i>';
+
+                // Show the span again
+                dd_element.style.display = '';
+            };
+        }
+
+    };
 };
 
 /**
@@ -172,45 +185,89 @@ window.onload = function () {
  * @param {*} _id
  */
 function submitSaveChanges(_id) {
+    let objectToEdit = hrefSplit[4];
     let form = document.createElement('form');
-    form.setAttribute('action', `/admin/categories/${_id}/edit`);
-    form.setAttribute('method', 'POST');
-    form.setAttribute('enctype', 'multipart/form-data');
 
-    let cate_id = document.createElement('input');
-    cate_id.setAttribute('type', 'hidden');
-    cate_id.setAttribute('name', '_id');
-    cate_id.value = _id;
+    switch (objectToEdit) {
+        case 'categories':
+            form.setAttribute('action', `/admin/categories/${_id}/edit`);
+            form.setAttribute('method', 'POST');
+            form.setAttribute('enctype', 'multipart/form-data');
 
-    let cate_name = document.createElement('input');
-    cate_name.setAttribute('type', 'hidden');
-    cate_name.setAttribute('name', 'category_name');
-    cate_name.value = document.getElementById('_category_name').textContent;
+            let cate_id = document.createElement('input');
+            cate_id.setAttribute('type', 'hidden');
+            cate_id.setAttribute('name', '_id');
+            cate_id.value = _id;
 
-    let cate_slug = document.createElement('input');
-    cate_slug.setAttribute('type', 'hidden');
-    cate_slug.setAttribute('name', 'slug');
-    cate_slug.value = document.getElementById('_slug').textContent;
+            let cate_name = document.createElement('input');
+            cate_name.setAttribute('type', 'hidden');
+            cate_name.setAttribute('name', 'category_name');
+            cate_name.value = document.getElementById('_category_name').textContent;
 
-    let cate_total = document.createElement('input');
-    cate_total.setAttribute('type', 'hidden');
-    cate_total.setAttribute('name', 'total');
-    cate_total.value = document.getElementById('_total').textContent;
+            let cate_slug = document.createElement('input');
+            cate_slug.setAttribute('type', 'hidden');
+            cate_slug.setAttribute('name', 'slug');
+            cate_slug.value = document.getElementById('_slug').textContent;
 
-    let cate_visible = document.createElement('input');
-    cate_visible.setAttribute('type', 'hidden');
-    cate_visible.setAttribute('name', 'visible');
-    cate_visible.value = document.getElementById('_visible').textContent;
+            let cate_total = document.createElement('input');
+            cate_total.setAttribute('type', 'hidden');
+            cate_total.setAttribute('name', 'total');
+            cate_total.value = document.getElementById('_total').textContent;
 
-    let cate_img = document.getElementById('gallery-photo-add');
+            let cate_visible = document.createElement('input');
+            cate_visible.setAttribute('type', 'hidden');
+            cate_visible.setAttribute('name', 'visible');
+            cate_visible.value = document.getElementById('_visible').textContent;
 
-    form.appendChild(cate_id);
-    form.appendChild(cate_name);
-    form.appendChild(cate_slug);
-    form.appendChild(cate_total);
-    form.appendChild(cate_visible);
-    form.appendChild(cate_img);
+            let cate_img = document.getElementById('gallery-photo-add');
 
-    document.body.appendChild(form);
-    form.submit();
+            form.appendChild(cate_id);
+            form.appendChild(cate_name);
+            form.appendChild(cate_slug);
+            form.appendChild(cate_total);
+            form.appendChild(cate_visible);
+            form.appendChild(cate_img);
+
+            document.body.appendChild(form);
+            form.submit();
+            break;
+
+        case 'products':
+            form.setAttribute('action', `/admin/products/${_id}/edit`);
+            form.setAttribute('method', 'POST');
+            form.setAttribute('enctype', 'multipart/form-data');
+            form.setAttribute('style', 'display:none;');
+
+            let gameName = document.createElement('input');
+            gameName.setAttribute('name', 'gameName');
+            gameName.value = document.getElementById('_gameName').textContent;
+
+            let price = document.createElement('input');
+            price.setAttribute('name', 'price');
+            price.value = document.getElementById('_price').textContent;
+
+            let champ = document.createElement('input');
+            champ.setAttribute('name', 'champ');
+            champ.value = document.getElementById('_champ').textContent;
+
+            let skin = document.createElement('input');
+            skin.setAttribute('name', 'skin');
+            skin.value = document.getElementById('_skin').textContent;
+
+            let visible = document.createElement('input');
+            visible.setAttribute('name', 'visible');
+            visible.value = document.getElementById('_visible').textContent;
+
+            let image = document.getElementById('image-gallery-add');
+
+            form.appendChild(gameName);
+            form.appendChild(price);
+            form.appendChild(champ);
+            form.appendChild(visible);
+            form.appendChild(image);
+
+            document.body.appendChild(form);
+            form.submit();
+            break;
+    }
 }
